7e7336bce6222109fa7a0cafd4601b4fa48d4b4e
aysikh <amy.sikhammountry@gmail.com>
2020-08-26 22:13:24 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
+# class Book
+#     def initialize(title) #initialize is a place we'll probably be doing some setup in
+#         @title = title
+#     end
+#     def title
+#         @title
+#     end
+#     def author=(author)
+#         @author = author
+#     end
+#     def author
+#         @author
+#     end
+#     def page_count=(num)
+#         @page_count = num
+#     end
+#     def page_count
+#         @page_count
+#     end
+#     def genre=(genre)
+#         @genre = genre
+#     end
+#     def genre
+#         @genre
+#     end
+#     def turn_page
+#         puts "Flipping the page...wow, you read fast!"
+#     end
+# end
 
+# book = Book.new("title")
+# book.turn_page
 
+class Book
+    attr_accessor :author, :genre, :page_count
+    attr_reader :title
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+book = Book.new("title")
+book.turn_page 
